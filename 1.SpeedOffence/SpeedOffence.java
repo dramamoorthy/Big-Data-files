27092702b49657b8339e183a73239cee67cfb49e
Dinesh Ramamoorthy <dineshr4981@gmail.com>
2017-11-21 17:47:54 +0530

    Add files via upload

---
+++ 1.SpeedOffence/SpeedOffence.java
@@ -0,0 +1,75 @@
+
+import java.io.*;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+
+public class SpeedOffence {
+	
+	public static class MapClass extends Mapper<LongWritable,Text,Text,LongWritable>
+	   {
+	      public void map(LongWritable key, Text value, Context context)
+	      {	    	  
+	         try{
+	            String[] str = value.toString().split(",");	 
+	            long vol = Long.parseLong(str[1]);
+	            context.write(new Text(str[0]),new LongWritable(vol));
+	         }
+	         catch(Exception e)
+	         {
+	            System.out.println(e.getMessage());
+	         }
+	      }
+	   }
+	
+	  public static class ReduceClass extends Reducer<Text,LongWritable,Text,LongWritable>
+	   {
+		    private LongWritable result1 = new LongWritable();
+		    
+		    public void reduce(Text key,Iterable<LongWritable> values,Context context) throws IOException, InterruptedException {
+		    	long count1=0;
+				long count2=0;
+				double offencePercent;
+				double offencePercent1;
+		         for (LongWritable val : values)
+		         {
+		        	 count1++;
+		         if(val.get()>65)
+		         {
+		        count2++;	 
+		         }
+		         }
+		         offencePercent=count2*100; 
+		         offencePercent1=offencePercent/count1;
+		         
+		      result1.set((long) offencePercent1);	
+		         //result1.set(counter2);
+		      context.write(key, result1);
+		      //context.write(key, new LongWritable(sum));
+		      
+		    }
+	   }
+	  public static void main(String[] args) throws Exception {
+		    Configuration conf = new Configuration();
+		    //conf.set("name", "value")
+		    //conf.set("mapreduce.input.fileinputformat.split.minsize", "134217728");
+		    Job job = Job.getInstance(conf, "Volume Count");
+		    job.setJarByClass(StockVolume.class);
+		    job.setMapperClass(MapClass.class);
+		    //job.setCombinerClass(ReduceClass.class);
+		    job.setReducerClass(ReduceClass.class);
+		    //job.setNumReduceTasks(0);
+		    job.setOutputKeyClass(Text.class);
+		    job.setOutputValueClass(LongWritable.class);
+		    FileInputFormat.addInputPath(job, new Path(args[0]));
+		    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		    System.exit(job.waitForCompletion(true) ? 0 : 1);
+		  }
+}
